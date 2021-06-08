1fe1c1f680557d223fdb75f1de08184e2f06acc8
antoine leleu <antoine-41@live.fr>
2021-06-08 13:57:22 +0200

    Ajout de beveragescontroller, burgerscontroller

---
+++ WebApplicationBurger/Controllers/BeveragesController.cs
@@ -0,0 +1,22 @@
+﻿using Dal;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationBurger.Controllers
+{
+    public class BeveragesController : Controller
+    {
+        private readonly BurgerContext _context;
+        public BeveragesController(BurgerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Beverages.ToList());
+        }
+    }
+}
---
+++ WebApplicationBurger/Controllers/BurgersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClassLibrary;
+using Dal;
+
+namespace WebApplicationBurger.Controllers
+{
+    public class BurgersController : Controller
+    {
+        private readonly BurgerContext _context;
+
+        public BurgersController(BurgerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Burgers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Burgers.ToListAsync());
+        }
+
+        // GET: Burgers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var burger = await _context.Burgers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+
+            return View(burger);
+        }
+
+        // GET: Burgers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Burgers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Weight,BeefWeight,Id,Price,Name,Description")] Burger burger)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(burger);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(burger);
+        }
+
+        // GET: Burgers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var burger = await _context.Burgers.FindAsync(id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+            return View(burger);
+        }
+
+        // POST: Burgers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Weight,BeefWeight,Id,Price,Name,Description")] Burger burger)
+        {
+            if (id != burger.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(burger);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BurgerExists(burger.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(burger);
+        }
+
+        // GET: Burgers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var burger = await _context.Burgers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+
+            return View(burger);
+        }
+
+        // POST: Burgers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var burger = await _context.Burgers.FindAsync(id);
+            _context.Burgers.Remove(burger);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BurgerExists(int id)
+        {
+            return _context.Burgers.Any(e => e.Id == id);
+        }
+    }
+}
